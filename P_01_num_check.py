19f238fb4ab11126da85995fb0253d687eaa30a3
CruelEden <mayesp50477@masseyhigh.school.nz>
2026-06-09 10:38:37 +1200

    number checker

--- P_01_num_check.py
+++ P_01_num_check.py
@@ -1,30 +1,56 @@
 # see if it's a valid number
+from http.client import responses
+distance_dict = {
+    "mm": 1000,
+    "cm": 100,
+    "m": 1,
+    "km": .001
+}
+
 
 def num_check(question):
-
-    error = "please enter number that is more then zero\n"
+    error = "Please enter a number that is more than zero (or 'xxx' to exit)\n"
     while True:
+        response = input(question).lower()  # Get input as text first
 
+        # 1. Check for the exit code first
+        if response == "xxx":
+            return response
+
+        # 2. If not exiting, try converting to a number
         try:
-            # ask the user for a number
-            response = float(input(question))
+            response = float(response)
 
-            # check that the number is more than zero
+            # Check that the number is more than zero
             if response > 0:
                 return response
             else:
                 print(error)
-            if response == "xxx":
-                return response
 
         except ValueError:
             print(error)
 
 
 # Main Routine Goes Here
-for item in range(0, 20):
-    amount = num_check("how much? ")
-
 while True:
-    if amount =="xxx":
+    amount = num_check("how much? ") or float(input("how much? "))
+    if amount == "xxx":
+        print("Exiting program. Goodbye!")
         break
+    from_units = input("From unit? ")
+    to_unit = input("To unit? ")
+
+    # Multiply to get the standard value...
+    multiply_by = distance_dict[to_unit]
+    standard = amount * multiply_by
+
+    # Divide to get our desired value
+    divide_by = distance_dict[from_units]
+    answer = standard / divide_by
+
+    print(f"There are {answer} {to_unit} in {amount} {from_units} ")
+
+
+
+
+
